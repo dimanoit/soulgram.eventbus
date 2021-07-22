72f85696bab45230b42255e52edeeccc6423731a
dimanoit <dimanoit@gmail.com>
2021-07-22 20:08:08 +0300

    feat: add base interfaces

---
+++ soulgram.eventbus/IntegrationEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Soulgram.Eventbus
+{
+	public record IntegrationEvent
+	{
+		public IntegrationEvent()
+		{
+			Id = Guid.NewGuid();
+			CreationDate = DateTime.UtcNow;
+		}
+
+		public IntegrationEvent(Guid id, DateTime createDate)
+		{
+			Id = id;
+			CreationDate = createDate;
+		}
+
+		public Guid Id { get; private init; }
+
+		public DateTime CreationDate { get; private init; }
+	}
+}
